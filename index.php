fed3c977e40f1a78255e5d3997ec49378480dad6
codebyamaury <codebyamaury@users.noreply.github.com>
2026-03-07 14:23:48 -0500

    fix: corregir doble query string en index.css dentro de index.php para forzar correcta animación neon en la palabra TECNICOS

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $productos_destacados = $stmt->fetchAll();
 
 // Título y CSS extra para esta página (Flowbite + index.css)
 $page_title = 'Inicio';
-    $extra_css = '<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />' . "\n" . '<link rel="stylesheet" href="' . asset('css/index.css') . '?v=' . time() . '_14">' . "\n" . '<link rel="stylesheet" href="' . asset('css/productos.css') . '?v=' . time() . '_7">';
+    $extra_css = '<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />' . "\n" . '<link rel="stylesheet" href="' . base_url() . '/assets/css/index.css?v=' . time() . '_16">' . "\n" . '<link rel="stylesheet" href="' . base_url() . '/assets/css/productos.css?v=' . time() . '_8">';
 
 // Incluir header común
 include __DIR__ . '/includes/header.php';
