0d613e97b9524caf97ee12c7d3b9baa6ce719994
codebyamaury <codebyamaury@users.noreply.github.com>
2026-03-10 10:28:43 -0500

    Admin logo text: sync with hero style from main page (energy-flow gradient, futuristic glow)

--- admin/_layout.php
+++ admin/_layout.php
@@ -86,7 +86,7 @@ $nav_items = [
                     <span class="admin-logo-power-wrap">
                         <i data-lucide="power" class="admin-logo-icon" style="width:32px;height:32px"></i>
                     </span>
-                    <span class="admin-logo-text">COMPU<em>TECNICOS</em></span>
+                    <span class="admin-logo-text">COMPU<span data-text="TECNICOS">TECNICOS</span></span>
                 </a>
 
                 <nav class="admin-nav">
